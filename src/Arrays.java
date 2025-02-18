d757b5efc1d458242fdfe7f6b92199b24c212bbf
CristianAndriel <cristianandrielsilva@gmail.com>
2025-02-17 21:29:04 -0300

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Arrays {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[3];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+
+        System.out.println(java.util.Arrays.toString(ninja)); /* O Arrays.toString() é um metodo
+        da classe Arrays em Java, utilizado para transformar um array (um conjunto de elementos)
+        em uma representação de string. Ou seja, ele converte o conteúdo do array para uma string que pode ser facilmente visualizada e manipulada.
+        É bem útil para quando você precisa imprimir ou visualizar os valores de um array de forma mais legível. */
+
+        //Simplificando: Imprime todos os dados da minha Array
+        }
+    }
